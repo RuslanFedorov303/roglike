bc1482e5073f17ce9415e0397d18b1f62e8afd67
RuslanFedorov303 <RuslanFedorov303@gmail.com>
2025-12-01 18:51:40 +0200

    Update classes.py

--- Рогалик/classes.py
+++ Рогалик/classes.py
@@ -1,7 +1,37 @@
 import pygame
+import random
 import  time
+
+
 bullets = []
 
+
+
+# ---------- Пулі ---------- #
+class Bullet:
+    def __init__(self, x, y, direction, comand, damage, speed=8):
+        self.rect = pygame.Rect(x, y, 10, 10)
+        self.direction = direction
+        self.speed = speed
+        self.comand = comand
+        self.damage = damage
+        self.img = pygame.image.load("Bullet.png")
+
+        if self.direction == "up":      self.img = pygame.transform.rotate(self.img, 90)
+        elif self.direction == "down":  self.img = pygame.transform.rotate(self.img, 270)
+        elif self.direction == "left":  self.img = pygame.transform.rotate(self.img, 180)
+        elif self.direction == "right": self.img = pygame.transform.rotate(self.img, 0)
+
+
+    def update(self):
+        if self.direction == "up":      self.rect.y -= self.speed
+        elif self.direction == "down":  self.rect.y += self.speed
+        elif self.direction == "left":  self.rect.x -= self.speed
+        elif self.direction == "right": self.rect.x += self.speed
+
+
+    def draw(self, screen):
+        screen.blit(self.img, (self.rect.left, self.rect.top))
 
 
 
@@ -21,11 +51,12 @@ class Object:
 
 # ---------- Опоненти та игрок ---------- #
 class Entiti(Object):
-    def __init__(self, x, y, width, height, img, hp, damage, defoult_directore="left"):
+    def __init__(self, x, y, width, height, img, hp, damage, comand, defoult_directore="left"):
         super().__init__(x, y, width, height, img)
         self.hp = hp
         self.damage = damage
         self.directore = defoult_directore
+        self.comand = comand
         self.start_time = time.time()
         self.end_time = 0
 
@@ -37,37 +68,83 @@ class Entiti(Object):
         duration = self.end_time - self.start_time
 
         if duration >= 0.7:
-            if direction == 'up': bullets.append(Bullet(self.rect.centerx, self.rect.centery, "up"))
-            elif direction == 'down': bullets.append(Bullet(self.rect.centerx, self.rect.centery, "down"))
-            elif direction == 'left': bullets.append(Bullet(self.rect.centerx, self.rect.centery, "left"))
-            elif direction == 'right': bullets.append(Bullet(self.rect.centerx, self.rect.centery, "right"))
+            if direction == 'up':      bullets.append(Bullet(self.rect.centerx, self.rect.centery, "up", self.comand, 30))
+            elif direction == 'down':  bullets.append(Bullet(self.rect.centerx, self.rect.centery, "down", self.comand, 30))
+            elif direction == 'left':  bullets.append(Bullet(self.rect.centerx, self.rect.centery, "left", self.comand, 30))
+            elif direction == 'right': bullets.append(Bullet(self.rect.centerx, self.rect.centery, "right", self.comand, 30))
             self.start_time = time.time()
 
 
-
-# ---------- Пулі ---------- #
-class Bullet:
-    def __init__(self, x, y, direction, speed=8):
-        self.rect = pygame.Rect(x, y, 10, 10)
-        self.direction = direction
-        self.speed = speed
-        self.img = pygame.image.load("Bullet.png")
-
-        if self.direction == "up": self.img = pygame.transform.rotate(self.img, 90)
-        elif self.direction == "down": self.img = pygame.transform.rotate(self.img, 270)
-        elif self.direction == "left": self.img = pygame.transform.rotate(self.img, 180)
-        elif self.direction == "right": self.img = pygame.transform.rotate(self.img, 0)
+    def collide_bullets(self, bullets_c, tanks):
+        global bullets
+        for bullet in bullets_c:
+            for tank in tanks:
+                if bullet.rect.colliderect(tank.rect) and bullet.comand != tank.comand:
+                    tank.hp -= bullet.damage
+                    bullets.remove(bullet)
 
 
-    def update(self):
-        if self.direction == "up": self.rect.y -= self.speed
-        elif self.direction == "down": self.rect.y += self.speed
-        elif self.direction == "left": self.rect.x -= self.speed
-        elif self.direction == "right": self.rect.x += self.speed
+
+class Bot(Entiti):
+    def __init__(self, x, y, width, height, img, hp, damage, comand, defoult_directore="left"):
+        super().__init__(x, y, width, height, img, hp, damage, comand, defoult_directore="left")
+        self.start_time_botMove = time.time()
+        self.end_time_botMove = 0
+        self.directore = defoult_directore
+        self.tank_image = img
+        self.directories = ["up", "down", "left", "right"]
 
 
-    def draw(self, screen):
-        screen.blit(self.img, (self.rect.left, self.rect.top))
+    def bot_random_rotate(self):
+        self.end_time_botMove = time.time()
+        duration = self.end_time_botMove - self.start_time_botMove
+
+
+        if self.directore == 'up':
+            if not self.rect.y < 0:
+                self.rect.y -= 3
+
+            new_img = pygame.transform.rotate(self.tank_image, 90)
+            self.img = new_img
+
+        elif self.directore == 'down':
+            if not self.rect.y > 600:
+                self.rect.y += 3
+
+            new_img = pygame.transform.rotate(self.tank_image, 270)
+            self.img = new_img
+
+        elif self.directore == 'left':
+            if not self.rect.x < 0:
+                self.rect.x -= 3
+
+            new_img = pygame.transform.rotate(self.tank_image, 180)
+            self.img = new_img
+
+        elif self.directore == 'right':
+            if not self.rect.x > 1200:
+                self.rect.x += 3
+
+            new_img = pygame.transform.rotate(self.tank_image, 0)
+            self.img = new_img
+
+
+        if duration > 1:
+            self.directore = random.choice(self.directories)
+            self.start_time_botMove = time.time()
+
+
+    def bot_move(self):
+    #    def colide_wall():
+        if self.directore == "up":      self.rect.y -= 3
+        elif self.directore == "down":  self.rect.y += 3
+        elif self.directore == "left":  self.rect.x -= 3
+        elif self.directore == "right": self.rect.x += 3
+
+
+
+
+
 
 
 
