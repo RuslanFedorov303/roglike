d2e3407a677f8103ed9489c71ea527af04d50907
RuslanFedorov303 <RuslanFedorov303@gmail.com>
2025-12-04 22:42:30 +0200

    Update code.py

--- Рогалик/code.py
+++ Рогалик/code.py
@@ -1,17 +1,19 @@
+import pygame.font
+
 from classes import *
 
 
 
 player_img = pygame.image.load("Green_tank.png")
 player_image = pygame.transform.scale(player_img, (70, 70))
-player = Entiti(0, 200, 30, 30, player_image, 100, 30, "right")
+player = Entiti(0, 200, 30, 30, player_image, 100000000, 30, "green", "right")
 
 
 tank_img = pygame.image.load("Red_tank.png")
 tank_image = pygame.transform.scale(tank_img, (70, 70))
-tank1 = Bot(1000, 200, 30, 30, tank_image, 100, 30, 'red')
-tank2 = Bot(1000, 300, 30, 30, tank_image, 100, 30, 'red')
-tank3 = Bot(1000, 400, 30, 30, tank_image, 100, 30, 'red')
+tank1 = Bot(1000, 200, 30, 30, tank_image, 100, 30, "red")
+tank2 = Bot(1000, 300, 30, 30, tank_image, 100, 30, "red")
+tank3 = Bot(1000, 400, 30, 30, tank_image, 100, 30, "red")
 
 all_tanks = [player, tank1, tank2, tank3]
 tanks =[tank1, tank2, tank3]
@@ -19,7 +21,7 @@ tanks =[tank1, tank2, tank3]
 
 wall_img = pygame.image.load("Wall.png")
 wall_image = pygame.transform.scale(wall_img, (40, 40))
-wall1 = Object(500, 300, 30, 30, wall_image)
+wall1 = Object(500, 300, 1, 1, wall_image)
 
 walls = [wall1]
 
@@ -28,9 +30,115 @@ img_fon = pygame.image.load("BG.jpg")
 fon = Object(0, 0, 1000, 1000, img_fon)
 
 
+font = Label(550, 300, 80, "Pause")
+# button_menu = Button(600, 350, "Вийти в меню", 40)
+
+
+
+
+
+def pause():
+    while True:
+        # обробка подій
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_ESCAPE:
+                    return
+
+
+
+    #    button_menu.draw()
+
+        font.draw(screen)
+        pygame.display.flip()
+        clock.tick(50)
+
+
+
+def game():
+    while True:
+        # обробка подій
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_ESCAPE:
+                    pause()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                player.shot(player.directore)
+
+
+        # відображення
+        if player.hp <= 0:
+            return
+
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            new_img = pygame.transform.rotate(player_image, 180)
+            player.img = new_img
+            player.rect.x -= 3
+            player.directore = "left"
+
+        if keys[pygame.K_d]:
+            new_img = pygame.transform.rotate(player_image, 0)
+            player.img = new_img
+            player.rect.x += 3
+            player.directore = "right"
+
+        if keys[pygame.K_w]:
+            new_img = pygame.transform.rotate(player_image, 90)
+            player.img = new_img
+            player.rect.y -= 3
+            player.directore = "up"
+
+        if keys[pygame.K_s]:
+            new_img = pygame.transform.rotate(player_image, 270)
+            player.img = new_img
+            player.rect.y += 3
+            player.directore = "down"
+
+
+        fon.draw(screen)
+        player.colide_walls(walls, all_tanks)
+        player.draw(screen)
+
+
+        for tank in tanks:
+            if not tank.hp <= 0:
+                tank.bot_random_rotate()
+                tank.collide_bullets(bullets, all_tanks)
+                tank.shot(tank.directore)
+                tank.colide_walls(walls, all_tanks)
+                tank.draw(screen)
+
+
+        for bullet in bullets:
+            if len(bullets) > 99:
+                del bullets[0]
+            bullet.update()
+            bullet.draw(screen)
+
+
+        for wall in walls:
+            wall.draw(screen)
+
+        # оновлення дисплея
+        pygame.display.flip()
+        clock.tick(50)
+
+
+
+button_start = Button(600, 300, "START", 50)
+button_start.onclick(game)
+
 screen = pygame.display.set_mode((1300, 700))
 clock = pygame.time.Clock()
-
 
 
 running = True
@@ -38,67 +146,19 @@ while running:
     # обробка подій
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            pygame.quit()
+            exit()
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            player.shot(player.directore)
-
-
-    # відображення
-    if player.hp <= 0:
-        running = False
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_a]:
-        new_img = pygame.transform.rotate(player_image, 180)
-        player.img = new_img
-        player.rect.x -= 3
-        player.directore = "left"
-
-    if keys[pygame.K_d]:
-        new_img = pygame.transform.rotate(player_image, 0)
-        player.img = new_img
-        player.rect.x += 3
-        player.directore = "right"
-
-    if keys[pygame.K_w]:
-        new_img = pygame.transform.rotate(player_image, 90)
-        player.img = new_img
-        player.rect.y -= 3
-        player.directore = "up"
-
-    if keys[pygame.K_s]:
-        new_img = pygame.transform.rotate(player_image, 270)
-        player.img = new_img
-        player.rect.y += 3
-        player.directore = "down"
-
-
+    # game_part_round()
     fon.draw(screen)
-    player.draw(screen)
 
+    button_start.update()
+    button_start.draw(screen)
 
-    for tank in tanks:
-        if not tank.hp <= 0:
-            tank.bot_random_rotate()
-            tank.collide_bullets(bullets, all_tanks)
-            tank.shot(tank.directore)
-            tank.draw(screen)
-
-
-    for bullet in bullets:
-        if len(bullets) > 99:
-            del bullets[0]
-        bullet.update()
-        bullet.draw(screen)
-
-
-    for wall in walls:
-        wall.draw(screen)
-
-    # оновлення дисплея
+    # оновлення дисплея та обмеження частоти
     pygame.display.flip()
     clock.tick(50)
+
 
 
 pygame.quit()
