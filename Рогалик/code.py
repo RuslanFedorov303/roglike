04a7399ba8602b648cd1def0e9fea40f4dbe9e04
RuslanFedorov303 <RuslanFedorov303@gmail.com>
2025-12-14 22:02:02 +0200

    Update code.py

--- Рогалик/code.py
+++ Рогалик/code.py
@@ -4,26 +4,19 @@ from classes import *
 
 
 
+# ---------- Створюємо все не обхідне для гри ---------- #
 player_img = pygame.image.load("Green_tank.png")
 player_image = pygame.transform.scale(player_img, (70, 70))
-# player_defoult[0, 200, 30, 30, player_image, 100, 2, 1000, "green", "right"]
-player = Entiti(0, 200, 30, 30, player_image, 100, 5, 1000, "green", "right")
+player = Entiti(0, 200, 30, 30, player_image, 100, 2, 30, "green", "right")
+player.max_hp = player.hp
 
 
 tank_img = pygame.image.load("Red_tank.png")
 tank_image = pygame.transform.scale(tank_img, (70, 70))
-# tank1 = Bot(1000, 200, 30, 30, tank_image, 100, 30, "red")
-# tank2 = Bot(1000, 300, 30, 30, tank_image, 100, 30, "red")
-# tank3 = Bot(1000, 400, 30, 30, tank_image, 100, 30, "red")
-
-
 
 
 wall_img = pygame.image.load("Wall.png")
 wall_image = pygame.transform.scale(wall_img, (40, 40))
-wall1 = Object(500, 300, 1, 1, wall_image)
-
-walls = [wall1]
 
 
 img_fon = pygame.image.load("BG.jpg")
@@ -31,10 +24,7 @@ fon = Object(0, 0, 1000, 1000, img_fon)
 
 
 
-# button_menu = Button(600, 350, "Вийти в меню", 40)
-
-
-
+# ---------- Створюємо саму гру ---------- #
 round1 = Round(
     [
     [1000, 200, 30, 30, tank_image, 100, 1.5, 30, "red", "left"]
@@ -46,6 +36,7 @@ round1 = Round(
     (500, 420),
     (500, 460)
     ), player, fon, wall_image, player_image)
+
 
 
 round2 = Round(
@@ -61,6 +52,7 @@ round2 = Round(
     (500, 420),
     (550, 460)
     ), player, fon, wall_image, player_image)
+
 
 
 round3 = Round(
@@ -87,8 +79,6 @@ round3 = Round(
 
 
 
-
-
 bufs = {
     "hp": [10, 30, 70, 80, 110, 150, 200],
     "speed": [1, 1.5, 2, 2.5, 3],
@@ -97,14 +87,16 @@ bufs = {
 
 
 
-game = Game([round1, round2, round3], bufs, fon, player)
-game.cycle()
+game1 = Game([round1, round2, round3], bufs, fon, player)
 
 
-button_start = Button(600, 300, "START", 50)
-button_start.onclick(round1.game())
+
+button_start = Button(550, 300, "START", 200)
+button_start.onclick(game1.cycle)
 
 
+
+# ---------- Меню ---------- #
 running = True
 while running:
     # обробка подій
@@ -113,9 +105,10 @@ while running:
             pygame.quit()
             exit()
 
+        button_start.update(event)
 
     fon.draw(screen)
-    button_start.update()
+    button_start.draw(screen)
 
 
     # оновлення дисплея та обмеження частоти
